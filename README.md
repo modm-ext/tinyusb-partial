9caae140c5dfb022a3bcb2aa04d0b5e0d243750d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-09-18 22:17:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TinyUSB partial tree
+
+This is just a partial copy of the TinyUSB repository to reduce repository size.
+The following paths are extracted:
+
+- `LICENSE`
+- `src/`
+
+More paths may be added in the future.
+
+This repository is updated monthly by TravisCI:
+[![](https://travis-ci.org/modm-ext/tinyusb-partial.svg?branch=master)](https://travis-ci.org/modm-ext/tinyusb-partial)
---
+++ update.py
@@ -0,0 +1,35 @@
+# Script is tested on OS X 10.12
+# YOUR MILEAGE MAY VARY
+
+import sys
+import shutil
+import fnmatch
+import subprocess
+from pathlib import Path
+
+source_paths = [
+    "LICENSE",
+    "src/",
+]
+
+# clone the repository
+print("Cloning TinyUSB repository...")
+if not Path("tinyusb_src").exists():
+    subprocess.run("git clone --jobs=8 --recurse-submodules https://github.com/hathach/tinyusb.git tinyusb_src", shell=True)
+
+# remove the sources in this repo
+if Path("src").exists():
+    shutil.rmtree("src")
+
+print("Copying TinyUSB sources...")
+for pattern in source_paths:
+    for path in Path("tinyusb_src").glob(pattern):
+        dest = path.relative_to("tinyusb_src")
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        if path.is_dir():
+            shutil.copytree(path, dest)
+        else:
+            shutil.copy2(path, dest)
+
+print("Normalizing TinyUSB newlines and whitespace...")
+subprocess.run("sh ./post_script.sh > /dev/null 2>&1", shell=True)
